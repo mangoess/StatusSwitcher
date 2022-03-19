aa9c462e52ab9065b39028314dadee42623a01e3
mango <adamtamers@outlook.com>
2022-03-19 20:05:34 +0400

    Update discordstatus.py

--- discordstatus.py
+++ discordstatus.py
@@ -1,23 +1,28 @@
+# Made by ads#2000 (although im 90% sure that someones just gonna remove this line and take it as their own)
+# You can not use this code for commerical purposes unless allowed to by the rightful owner, ads#2000.
+
+# Make sure you have all of these modules installed by using pip :D
 import time
 import requests
 import json
 import sys
+
+first = '{"custom_status":{"text":"Skill tree application coming out in"}}'
+second = '{"custom_status":{"text":"November 2022"}}'
 url = 'https://discord.com/api/v9/users/@me/settings'
 contento = 'application/json'
-dataz = '{"custom_status":{"text":"November 2022"}}'
-dato = '{"custom_status":{"text":"Skill tree application coming out in"}}'
-auth = 'Put your discord token here'
+auth = 'Put your discord token here' # Search up how to find your discord token, once you have it, replace the text with your token.
 
-while True:
+while True: # The loop that switches the statuses, you don't have to touch anything in here
     header = {
         'authorization': auth,
         'content-type': "application/json"
         }
 
     r = requests.patch(url,
-                       data=dato, headers=header)
+                       data=first, headers=header)
 
     time.sleep(3)
     r = requests.patch(url,
-                       data=dataz, headers=header)
+                       data=second, headers=header)
     time.sleep(3)
